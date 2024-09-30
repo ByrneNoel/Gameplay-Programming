0f4b2e1f4152e8e53713432eb65ed87558902155
(Student  C00299761) Noel Byrne <C00299761@setu.ie>
2024-09-30 10:54:30 +0100

    On snippets

--- projects/practical_01/practical01/practical_01/snippets.cpp
+++ projects/practical_01/practical01/practical_01/snippets.cpp
@@ -14,7 +14,7 @@ int main()
 	int* pb = &b;
 	*pb = 30;
 
-	std::cout << "New value of b: " << b << "\n ";
+	std::cout << "New value of b: " << b << "\n";
 
 	int* dynamicArray = new int[5];
 	
@@ -27,5 +27,26 @@ int main()
 	}
 
 	delete[] dynamicArray;
+
+	std::cout << std::endl;
+
+	int arr[3] = { 10,20,30 };
+	int* pArr = arr;
+
+	for (int i = 0; i < 3; i++)
+	{
+		std::cout << "Element " << i << " is: " << *(pArr + i) << "\n";
+	}
+
+	int x = 5;
+	int* px = &x;
+
+	std::cout << "Address of x: " << px << "\n";
+	std::cout << "Value of x: " << *px << "\n";
+
+
+
+	return 1;
+
 
 }
