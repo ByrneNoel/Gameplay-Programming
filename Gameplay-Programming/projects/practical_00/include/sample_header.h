11fb5d8a30f3a6dbb9a72fc4f3950788f56b8064
ByrneNoel <noelbyrne2013@gmail.com>
2024-10-21 18:33:32 +0100

    fixing folder

--- Gameplay-Programming/projects/practical_00/include/sample_header.h
+++
@@ -1,1 +0,0 @@
-void printMakefileSuccess(void);
--- Gameplay-Programming/projects/practical_00/src/sample_function.cpp
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <sample_header.h>
-
-void printMakefileSuccess(void) {
-
-  printf("Hello makefiles!\n");
-
-  return;
-}
